394f2790cc4be726c652130093431250c7b5138b
Evan <evanfujita@gmail.com>
2020-12-13 16:00:59 -0600

    commit tree 0be3c475844ffa60948d7eb9bd99e4f328e7b506

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
+require 'pry'
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :song, :name, :artist_name
+  attr_reader :song
+
   @@all = []
 
   def self.all
@@ -7,7 +10,62 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    @name = name
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    @name = name
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+      
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    song = self.new
+    artist = filename.split(" - ")
+    song.artist_name = artist[0]
+    name = artist[1].split(".mp3")
+    #inding.pry
+    song.name = name[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+    
+    
+  end
+
+  def self.destroy_all
+    @@all = [ ]
   end
 
 end
+
+#binding.pry
